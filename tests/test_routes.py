77859fae2f3fe7e80d99187fc2af21741c8b17d6
betosalvestrini <betosalvestrini@gmail.com>
2023-02-14 13:11:00 -0500

    Added Method Not Allow Test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -184,3 +184,8 @@ class TestAccountService(TestCase):
             f"{BASE_URL}/{test_account.id}"
         )
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
